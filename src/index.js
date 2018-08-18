9c9860f0b1aa295066441ab2e9e74df241b53e72
svonidze <svonidze@mail.ru>
2018-08-18 15:25:17 +0300

    Implemnted assigning player markers: X or O

--- src/index.js
+++ src/index.js
@@ -2,41 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 
-
-class Square extends React.Component {
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={this.props.onClick}>
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            xIsNext: true,
         }
     }
 
-    handleClick(i){
+    getPlayerName(xIsNext) {
+        return xIsNext ? 'X' : 'O';
+    }
+
+    handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.getPlayerName(this.state.xIsNext);
+        this.setState({ 
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
     }
 
     renderSquare(i) {
-        return <Square 
+        return <Square
             value={this.state.squares[i]}
             onClick={() => this.handleClick(i)} />;
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = `Next player: ${this.getPlayerName(this.state.xIsNext)}`;
 
         return (
             <div>
